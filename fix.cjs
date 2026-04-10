c4db4655012cc0efdca528285b609158b8e45fbe
RomatecCRMWatsApp <romateccrm@gmail.com>
2026-04-09 21:54:04 -0300

    fix: remover arquivos temporarios

--- fix.cjs
+++
@@ -1,6 +0,0 @@
-﻿const mysql = require('mysql2/promise');
-mysql.createConnection({host:'mainline.proxy.rlwy.net',port:56439,user:'root',password:'mzhpVamVFtfKDLkQtfxGnjnlVLrVEaAf',database:'railway'}).then(async conn => {
-  await conn.execute('UPDATE campaigns SET totalContacts=2, sentCount=0, failedCount=0');
-  console.log('Atualizado!');
-  conn.end();
-});
