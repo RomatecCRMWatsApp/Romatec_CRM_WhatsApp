6dadc96e333c433c8383227e4ccf599aa671dcbf
RomatecCRMWatsApp <romateccrm@gmail.com>
2026-04-09 21:53:34 -0300

    fix: limpar banco campanhas

---
+++ fix.cjs
@@ -0,0 +1,6 @@
+﻿const mysql = require('mysql2/promise');
+mysql.createConnection({host:'mainline.proxy.rlwy.net',port:56439,user:'root',password:'mzhpVamVFtfKDLkQtfxGnjnlVLrVEaAf',database:'railway'}).then(async conn => {
+  await conn.execute('UPDATE campaigns SET totalContacts=2, sentCount=0, failedCount=0');
+  console.log('Atualizado!');
+  conn.end();
+});
---
+++ fix3.cjs
@@ -0,0 +1,6 @@
+﻿const mysql = require('mysql2/promise');
+mysql.createConnection({host:'mainline.proxy.rlwy.net',port:56439,user:'root',password:'mzhpVamVFtfKDLkQtfxGnjnlVLrVEaAf',database:'railway'}).then(async conn => {
+  const [tables] = await conn.execute('SHOW TABLES');
+  console.log(tables.map(t => Object.values(t)[0]).join('\n'));
+  conn.end();
+});
